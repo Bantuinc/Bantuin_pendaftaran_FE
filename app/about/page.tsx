7a37d34773725fb28da76e634efe0b1c5f8db754
Muhamad Fihris Aldama <fihrisaldama015@gmail.com>
2023-09-14 21:23:29 +0700

    [FIX] static import img

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -10,6 +10,8 @@ function AboutUs() {
           alt="Hero Login Image"
           fill
           className="z-[-1] object-cover"
+          sizes="{(max-width: 768px) 768px, (max-width: 1440px) 1440px, 100vw}"
+          priority={true}
         />
         <div className="flex px-9 py-16 ring-2 ring-slate-200/70 rounded-[25px] bg-gradient-to-tr from-[#ccc3] to-[#ccca] backdrop-blur-sm">
           <h1
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,16 +1,19 @@
 import Image from "next/image";
 import { hind } from "@/app/layout";
+import Background from "@/public/background.webp"
 
 function Hero() {
   return (
     <section className="p-12 pt-28 md:p-6 relative min-h-[110vh] flex flex-col-reverse md:flex-row gap-6 justify-center items-center">
       <Image
-        src="/background.webp"
+        src={Background}
         alt="Hero Login Image"
         fill
         className="z-[-1] object-cover"
         sizes="{(max-width: 768px) 768px, (max-width: 1440px) 1440px, 100vw}"
         priority={true}
+        placeholder="blur"
+        quality={50}
       />
       <div className="flex px-9 py-16 ring-2 ring-slate-200/70 rounded-[25px] bg-gradient-to-tr from-[#ccc3] to-[#ccca] backdrop-blur-sm">
         <Image src="/logo2.png" height={300} width={400} alt="Hero" />
