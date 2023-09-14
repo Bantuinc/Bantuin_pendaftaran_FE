fadb530fd3eb2d25f1cba02a72687d3829096d4a
Muhamad Fihris Aldama <fihrisaldama015@gmail.com>
2023-09-14 20:48:15 +0700

    [FIX] responsive image

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,13 +4,15 @@ import { hind } from "../layout";
 function Login() {
   return (
     <section
-      className={`relative min-h-[110vh] p-12 pt-24 flex gap-6 justify-end items-center ${hind.className} antialiased`}
+      className={`relative min-h-[110vh] p-12 pt-24 flex gap-6 justify-end items-center ${hind.className} antialiased overflow-hidden`}
     >
       <Image
-        src="/background_login.webp"
+        src="/bg_login.webp"
         alt="Hero Login Image"
         fill
         className="object-cover"
+        sizes="{(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw}"
+        priority={true}
       />
       <div className="flex flex-col px-9 py-16 sm:w-[28rem] ring-2 ring-slate-200/70 rounded-[25px] bg-gradient-to-tr from-[#ccc8] to-[#ccca] backdrop-blur-[12px] text-white">
         <h1 className="mb-12 text-center text-4xl font-semibold drop-shadow-md">
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -9,6 +9,8 @@ function Hero() {
         alt="Hero Login Image"
         fill
         className="z-[-1] object-cover"
+        sizes="{(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw}"
+        priority={true}
       />
       <div className="flex px-9 py-16 ring-2 ring-slate-200/70 rounded-[25px] bg-gradient-to-tr from-[#ccc3] to-[#ccca] backdrop-blur-sm">
         <Image src="/logo2.png" height={300} width={400} alt="Hero" />
