a868ed53926b5f49407512338d57bab0f59c703f
Muhamad Fihris Aldama <fihrisaldama015@gmail.com>
2023-09-25 00:56:03 +0700

    [ADD] faq content

--- components/Faq.tsx
+++ components/Faq.tsx
@@ -1,4 +1,4 @@
-import { cocogoose } from "@/fonts/font";
+import { cocogoose, hind } from "@/fonts/font";
 import Underline from "./Underline";
 import {
   Accordion,
@@ -28,8 +28,17 @@ function Faq() {
               </AccordionTrigger>
               <AccordionContent>
                 <div className="px-10 py-5">
-                  ISMC is a competition for mining engineering students in
-                  Indonesia. This competition is held by ITB.
+                  ISMC, or Indonesian Student Mining Competition, is an
+                  international-scale student mining competition stemming from
+                  the experience of the HMT-ITB team participating in the AusIMM
+                  Students Mining Competition held by the University of
+                  Queensland (UQ) in{" "}
+                  <span className={`${hind.className}`}>1996</span> in Brisbane,
+                  Australia. In{" "}
+                  <span className={`${hind.className}`}>2024</span>, ISMC will
+                  be held for the{" "}
+                  <span className={`${hind.className}`}>14</span>
+                  <sup>th</sup> time.
                 </div>
               </AccordionContent>
             </AccordionItem>
@@ -39,12 +48,55 @@ function Faq() {
           <Accordion type="single" collapsible>
             <AccordionItem value="item-2" className="border-b-0">
               <AccordionTrigger className="px-10 py-5 text-[#425B5F] font-medium bg-[#9FB9B9] border-2 border-gray-400 rounded-3xl text-xl hover:no-underline">
-                What is ISMC?
+                What are the events?
               </AccordionTrigger>
               <AccordionContent>
                 <div className="px-10 py-5">
-                  ISMC is a competition for mining engineering students in
-                  Indonesia. This competition is held by ITB.
+                  There will be many immaculate events, including the Mining
+                  Competition, which consists of{" "}
+                  <span className={`${hind.className}`}>16</span> kinds of
+                  practical mining competitions: paper contests, poster
+                  contests, hackathon contests, mining exhibitions and
+                  exhibitions, mining talks, youth mining conferences, and more.
+                  We also provide spectacular prizes for the winners of our
+                  competition and contest.
+                </div>
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
+        </div>
+        <div className="bg-[#F8CF9D] rounded-3xl">
+          <Accordion type="single" collapsible>
+            <AccordionItem value="item-1" className="border-b-0">
+              <AccordionTrigger className="px-10 py-5 text-[#425B5F] font-medium bg-[#FFA31D] border-2 border-yellow-400 rounded-3xl text-xl hover:no-underline">
+                When will ISMC be held?
+              </AccordionTrigger>
+              <AccordionContent>
+                <div className="px-10 py-5">
+                  The <span className={`${hind.className}`}>14</span>
+                  <sup>th</sup> ISMC will be held from February{" "}
+                  <span className={`${hind.className}`}>18</span>
+                  <sup>th</sup> to February{" "}
+                  <span className={`${hind.className}`}>25</span>
+                  <sup>th</sup>,{" "}
+                  <span className={`${hind.className}`}>2024</span>. But
+                  registration is now open.
+                </div>
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
+        </div>
+        <div className="bg-[#F8CF9D] rounded-3xl">
+          <Accordion type="single" collapsible>
+            <AccordionItem value="item-2" className="border-b-0">
+              <AccordionTrigger className="px-10 py-5 text-[#425B5F] font-medium bg-[#9FB9B9] border-2 border-gray-400 rounded-3xl text-xl hover:no-underline">
+                Where will the ISMC be held?
+              </AccordionTrigger>
+              <AccordionContent>
+                <div className="px-10 py-5">
+                  The <span className={`${hind.className}`}>14</span>
+                  <sup>th</sup> ISMC will be held in Bandung, Indonesia,
+                  especially at the Bandung Institute of Technology.
                 </div>
               </AccordionContent>
             </AccordionItem>
