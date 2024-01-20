537c2420153090cc6c403f8ec0d5284974b744a6
Muhamad Fihris Aldama <fihrisaldama015@gmail.com>
2024-01-20 21:58:21 +0700

    [FIX] logo M > L, posisi logo L geser ke kiri

--- app/(index)/partnership/page.tsx
+++ app/(index)/partnership/page.tsx
@@ -16,7 +16,7 @@ function page() {
         <h1 className="text-6xl mb-6 py-2 text-center font-extrabold bg-gradient-to-r from-[#fffa] to-[#FFF] text-transparent bg-clip-text">
           Our Sponsor
         </h1>
-        <div className="flex flex-col items-center gap-3 px-6 py-6 ring-2 ring-slate-200/70 rounded-[32px] bg-gradient-to-tr from-[#ccc6] to-[#cccc] backdrop-blur-[12px] md:w-[80vw] w-full shadow-lg">
+        <div className="flex flex-col items-center gap-3 sm:px-6 px-3 py-6 ring-2 ring-slate-200/70 rounded-[32px] bg-gradient-to-tr from-[#ccc6] to-[#cccc] backdrop-blur-[12px] md:w-[90vw] w-full shadow-lg">
           <p className="font-bold text-slate-900 text-sm">SPONSORED BY</p>
           <div className="flex flex-col gap-0 items-center w-full">
             {/* XXL */}
@@ -25,81 +25,81 @@ function page() {
               alt="MAA"
               width={1600}
               height={1200}
-              className="h-[10rem] object-center w-full object-contain  rounded-xl"
+              className="h-[10rem] object-center w-full object-contain rounded-xl"
             />
             {/* XXL */}
             {/* XL */}
-            <div className="flex justify-center sm:gap-6 items-center flex-wrap">
+            <div className="mb-3 flex justify-center sm:gap-6 items-center flex-wrap">
               <img
                 src={"/sponsor/XL/pertamina.png"}
                 width={2339}
                 height={1654}
                 alt="pertamina"
-                className="w-auto md:h-[9rem] h-[6rem] object-contain rounded-2xl"
+                className="w-auto md:h-[8.5rem] h-[5.75rem] object-contain rounded-2xl"
               />
               <img
                 src={"/sponsor/XL/orica.png"}
                 width={2615}
                 height={1177}
                 alt="orica"
-                className="w-auto md:h-[9rem] h-[6rem] object-contain  rounded-xl"
+                className="w-auto md:h-[8.5rem] h-[5.75rem] object-contain  rounded-xl"
               />
             </div>
             {/* XL */}
             {/* L */}
             <div className="mb-3 flex justify-center sm:gap-12 gap-3 items-center flex-wrap">
               <img
-                src="/sponsor/M/aptekindo.png"
-                width={1455}
-                height={717}
-                alt="Aptekindo"
-                className="sm:h-[5.25rem] h-[3rem] rounded-lg w-auto object-contain"
-              />
-              <img
-                src="/sponsor/M/bib.png"
-                width={4207}
-                height={542}
-                alt="bib"
-                className="sm:h-[3.25rem] h-[2rem] w-auto rounded-lg object-contain"
-              />
-              <img
-                src="/sponsor/M/arutmin.png"
+                src="/sponsor/L/arutmin.png"
                 width={479}
                 height={512}
                 alt="arutmin"
-                className="sm:h-[5.25rem] h-[3rem] w-auto object-contain"
+                className="sm:h-[7rem] h-[4.5rem] sm:w-auto object-contain"
               />
               <img
                 src="/sponsor/L/migc.png"
                 width={440}
                 height={798}
                 alt="migc"
-                className="sm:h-[5.25rem] h-[3rem] w-[3rem] object-contain"
+                className="sm:h-[7rem] h-[4.5rem] sm:w-auto object-contain"
+              />
+              {/* L */}
+              {/* M */}
+              <img
+                src="/sponsor/M/bib.png"
+                width={4207}
+                height={542}
+                alt="bib"
+                className="sm:h-[2.5rem] h-[1.75rem] w-auto rounded-lg object-contain"
+              />
+              <img
+                src="/sponsor/M/aptekindo.png"
+                width={1455}
+                height={717}
+                alt="Aptekindo"
+                className="sm:h-[4.5rem] h-[3rem] rounded-lg w-auto object-contain"
               />
             </div>
-            {/* L */}
-            {/* M */}
-            <div className="mb-3 flex justify-center sm:gap-6 gap-3 items-center flex-wrap">
+            <div className="mb-6 flex justify-center sm:gap-6 gap-3 items-center flex-wrap">
               <img
                 src="/sponsor/M/mifa.png"
                 width={1294}
                 height={527}
                 alt="Mifa"
-                className="sm:h-[5rem] h-[3rem] rounded-lg w-auto object-contain"
+                className="sm:h-[4.5rem] h-[3rem] rounded-lg w-auto object-contain"
               />
               <img
-                src="/sponsor/S/antam.png"
+                src="/sponsor/M/antam.png"
                 width={2537}
                 height={933}
                 alt="antam"
-                className="sm:h-[5rem] h-[3rem] w-auto object-contain rounded-lg"
+                className="sm:h-[4.5rem] h-[3rem] w-auto object-contain rounded-lg"
               />
               <img
                 src="/sponsor/M/ptar.png"
                 width={1250}
                 height={1005}
                 alt="PTAR"
-                className="sm:h-[5rem] h-[3rem] rounded-lg w-auto object-contain"
+                className="sm:h-[4.5rem] h-[3rem] rounded-lg w-auto object-contain"
               />
             </div>
             {/* M */}
