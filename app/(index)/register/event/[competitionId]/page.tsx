677866f3067c78eaf2b4514b27d8b9f3cf479dbd
Muhamad Fihris Aldama <fihrisaldama015@gmail.com>
2024-02-02 16:17:14 +0700

    [FIX] event close/open logic

--- app/(index)/register/event/[competitionId]/page.tsx
+++ app/(index)/register/event/[competitionId]/page.tsx
@@ -18,6 +18,9 @@ const getTypeOfCompetition = async (
       headers: {
         Authorization: `Bearer ${token}`,
       },
+      next: {
+        revalidate: 60,
+      },
     }
   );
   const data: APIResponse = await res.json();
@@ -25,86 +28,94 @@ const getTypeOfCompetition = async (
 };
 
 async function page({ params }: { params: { competitionId: string } }) {
-  const Competition = await getTypeOfCompetition(
-    params.competitionId,
-    ""
-  );
+  const Competition = await getTypeOfCompetition(params.competitionId, "");
 
-  const additionalFields = await getAdditionalField(
-    "",
-    Competition.id
-  );
+  const additionalFields = await getAdditionalField("", Competition.id);
   if (additionalFields === null) {
     redirect("/login");
   }
   const dateTimeClose = new Date(Competition.closedAt);
   const dateTimeNow = new Date(Date.now());
-  if(dateTimeClose < dateTimeNow){
+  if (dateTimeClose < dateTimeNow) {
     // KOMPETISI SUDAH BERAKHIR
     return (
-        <section
-            className={`relative min-h-[110vh] p-12 pt-28 flex gap-6 md:justify-end justify-center items-center ${hind.className} antialiased overflow-hidden`}
+      <section
+        className={`relative min-h-[110vh] p-12 pt-28 flex gap-6 md:justify-end justify-center items-center ${hind.className} antialiased overflow-hidden`}
+      >
+        <Image
+          src={Background}
+          alt="Hero Login Image"
+          fill
+          className="object-cover"
+          sizes="{(max-width: 768px) 768px, (max-width: 1440px) 1440px, 100vw}"
+          priority={true}
+          placeholder="blur"
+          quality={50}
+        />
+        <div
+          className="p-20 absolute left-0 w-fit md:block hidden rounded-r-[75px] bg-[#D9D9D9]"
+          style={{ boxShadow: "-20vw 0 0 #d9d9d9" }}
         >
           <Image
-              src={Background}
-              alt="Hero Login Image"
-              fill
-              className="object-cover"
-              sizes="{(max-width: 768px) 768px, (max-width: 1440px) 1440px, 100vw}"
-              priority={true}
-              placeholder="blur"
-              quality={50}
+            src="/logo2.png"
+            height={300}
+            width={400}
+            alt="Hero"
+            priority
           />
-          <div
-              className="p-20 absolute left-0 w-fit md:block hidden rounded-r-[75px] bg-[#D9D9D9]"
-              style={{ boxShadow: "-20vw 0 0 #d9d9d9" }}
-          >
-            <Image src="/logo2.png" height={300} width={400} alt="Hero" priority />
-          </div>
-          <div className="flex flex-col px-9 py-16 sm:w-[28rem] ring-2 ring-slate-200/70 rounded-[25px] bg-gradient-to-tr from-[#ccc8] to-[#ccca] backdrop-blur-[12px] text-white">
-            <h1 className="mb-12 text-center text-4xl font-semibold drop-shadow-md">
-              {Competition?.name} Registration
-              <span className="text-[#FFA31D] mb-12 text-center text-3xl font-semibold drop-shadow-md"><br/><br/> Registration is Closed</span>
-            </h1>
-          </div>
-        </section>
+        </div>
+        <div className="flex flex-col px-9 py-16 sm:w-[28rem] ring-2 ring-slate-200/70 rounded-[25px] bg-gradient-to-tr from-[#ccc8] to-[#ccca] backdrop-blur-[12px] text-white">
+          <h1 className="mb-12 text-center text-4xl font-semibold drop-shadow-md">
+            {Competition?.name} Registration
+            <span className="text-[#FFA31D] mb-12 text-center text-3xl font-semibold drop-shadow-md">
+              <br />
+              <br /> Registration is Closed
+            </span>
+          </h1>
+        </div>
+      </section>
     );
-  }else{
+  } else {
     // KOMPETISI BELUM BERAKHIR
     return (
-        <section
-            className={`relative min-h-[110vh] p-12 pt-28 flex gap-6 md:justify-end justify-center items-center ${hind.className} antialiased overflow-hidden`}
+      <section
+        className={`relative min-h-[110vh] p-12 pt-28 flex gap-6 md:justify-end justify-center items-center ${hind.className} antialiased overflow-hidden`}
+      >
+        <Image
+          src={Background}
+          alt="Hero Login Image"
+          fill
+          className="object-cover"
+          sizes="{(max-width: 768px) 768px, (max-width: 1440px) 1440px, 100vw}"
+          priority={true}
+          placeholder="blur"
+          quality={50}
+        />
+        <div
+          className="p-20 absolute left-0 w-fit md:block hidden rounded-r-[75px] bg-[#D9D9D9]"
+          style={{ boxShadow: "-20vw 0 0 #d9d9d9" }}
         >
           <Image
-              src={Background}
-              alt="Hero Login Image"
-              fill
-              className="object-cover"
-              sizes="{(max-width: 768px) 768px, (max-width: 1440px) 1440px, 100vw}"
-              priority={true}
-              placeholder="blur"
-              quality={50}
+            src="/logo2.png"
+            height={300}
+            width={400}
+            alt="Hero"
+            priority
           />
-          <div
-              className="p-20 absolute left-0 w-fit md:block hidden rounded-r-[75px] bg-[#D9D9D9]"
-              style={{ boxShadow: "-20vw 0 0 #d9d9d9" }}
-          >
-            <Image src="/logo2.png" height={300} width={400} alt="Hero" priority />
-          </div>
-          <div className="flex flex-col px-9 py-16 sm:w-[28rem] ring-2 ring-slate-200/70 rounded-[25px] bg-gradient-to-tr from-[#ccc8] to-[#ccca] backdrop-blur-[12px] text-white">
-            <h1 className="mb-12 text-center text-4xl font-semibold drop-shadow-md">
-              {Competition?.name} Registration
-            </h1>
-            <RegistEventForm
-                competitionId={params.competitionId}
-                competitionType={Competition?.type}
-                additionalFields={additionalFields}
-            />
-          </div>
-        </section>
+        </div>
+        <div className="flex flex-col px-9 py-16 sm:w-[28rem] ring-2 ring-slate-200/70 rounded-[25px] bg-gradient-to-tr from-[#ccc8] to-[#ccca] backdrop-blur-[12px] text-white">
+          <h1 className="mb-12 text-center text-4xl font-semibold drop-shadow-md">
+            {Competition?.name} Registration
+          </h1>
+          <RegistEventForm
+            competitionId={params.competitionId}
+            competitionType={Competition?.type}
+            additionalFields={additionalFields}
+          />
+        </div>
+      </section>
     );
   }
-
 }
 
 export default page;
